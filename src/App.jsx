4a8f08bccf893ee29addb12152bc25e324b2148b
Guruprasad3n <mr.guruprasadb@gmail.com>
2023-09-13 22:48:06 +0530

    Ecommerce

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 import "./App.css";
-// import BComponent from "./Components/BComponent";
+import BComponent from "./Components/BComponent";
 import { CardWithForm } from "./Components/Card";
 
 function App() {
